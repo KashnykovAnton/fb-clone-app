ecfba37c74ab5b7ca8da554d858a0105e9f91072
KashnykovAnton <kashnykov.anton@gmail.com>
2025-03-12 00:33:00 +0100

    render api added, changes in scripts and api.js done

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,13 @@ import axios from "axios";
 // old approach
 // axios.defaults.baseURL = REACT_APP_BASE_URL + ':' + REACT_APP_PORT;
 
-const REACT_APP_BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost";
-const REACT_APP_PORT = process.env.REACT_APP_PORT || "3030";
+// const REACT_APP_BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost";
+// const REACT_APP_PORT = process.env.REACT_APP_PORT || "3030";
+
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? `${process.env.REACT_APP_BASE_URL}:${process.env.REACT_APP_PORT}`
+    : `${process.env.REACT_APP_BASE_URL}/api`;
 
 export const API_ENDPOINTS = {
   POSTS: "posts",
@@ -14,7 +19,8 @@ export const API_ENDPOINTS = {
 };
 
 export const apiClient = axios.create({
-  baseURL: `${REACT_APP_BASE_URL}:${REACT_APP_PORT}`,
+  // baseURL: `${REACT_APP_BASE_URL}:${REACT_APP_PORT}`,
+  baseURL,
 });
 
 apiClient.interceptors.request.use(
